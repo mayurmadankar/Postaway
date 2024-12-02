01dc4d17e60e40844a5d22c1a74d6a4ecd820567
Mayur <madankarmayur5@gmail.com>
2024-12-02 23:20:12 +0530

    details

--- src/features/detais/detail.route.js
+++ src/features/detais/detail.route.js
@@ -5,7 +5,7 @@ import jwtAuth from "../../middleware/jwt.middleware.js";
 const detailRouter = express.Router();
 const detailController = new DetailController();
 
-detailRouter.get("/", jwtAuth, (req, res, next) => {
+detailRouter.get("/mine", jwtAuth, (req, res, next) => {
   detailController.detail(req, res, next);
 });
 export default detailRouter;
